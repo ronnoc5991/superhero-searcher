b280ade307aca9aeaf05a50ea432c9fe2734941f
Connor <cstreng95@gmail.com>
2020-08-08 15:20:59 +0200

    -

--- src/Hero.js
+++ src/Hero.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 
 function Hero (props) {
 
+    if (props.hero.name) {
+        console.log(props.hero);
+    }
+
     return (
-        <div>
+        <div className="hero-container" >
             { props.hero.name ? 
                 <>
                     <p> { props.hero.name } </p>
@@ -11,9 +15,9 @@ function Hero (props) {
                         <p> Gender: { props.hero.appearance.gender } </p>
                         <p> Eye Color: { props.hero.appearance.eyeColor } </p>
                         <p> Hair Color: { props.hero.appearance.hairColor } </p>
-                        <p> Height: { props.hero.appearance.height[0] } { props.hero.appearance.height[1] } </p>
+                        <p> Height: { props.hero.appearance.height[1] } </p>
                         <p> Race: { props.hero.appearance.race } </p>
-                        <p> Weight: { props.hero.appearance.weight[0] } { props.hero.appearance.weight[1] }</p>
+                        <p> Weight: { props.hero.appearance.weight[1] }</p>
                     </div>
                     <div className="biography">
                         <p> { props.hero.biography.fullName } </p>
