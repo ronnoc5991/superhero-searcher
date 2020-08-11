211a16f4ffd19c8121b38accccb0a2300549a485
Connor <cstreng95@gmail.com>
2020-08-11 11:23:32 +0200

    Updates

---
+++ precache-manifest.22727d8c6afe99ee0dbbcf1d55294c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a75985b4635ef2e5a6681ea580611a",
+    "url": "/superhero-searcher/index.html"
+  },
+  {
+    "revision": "6fdee0b466f66e971402",
+    "url": "/superhero-searcher/static/css/main.f89950a2.chunk.css"
+  },
+  {
+    "revision": "a75980a49b49aeb3b759",
+    "url": "/superhero-searcher/static/js/2.56a67395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superhero-searcher/static/js/2.56a67395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdee0b466f66e971402",
+    "url": "/superhero-searcher/static/js/main.4a3b2430.chunk.js"
+  },
+  {
+    "revision": "14fcdcd5d08dff5d1d43",
+    "url": "/superhero-searcher/static/js/runtime-main.84b295bd.js"
+  },
+  {
+    "revision": "a5f92e591e1d94a37e3241bad95e79b2",
+    "url": "/superhero-searcher/static/media/fingerprint3.a5f92e59.svg"
+  }
+]);
